fbeea7250a6bba40889ce18a3ac6cba2fb865c8e
Ryuma Noma <ryuma20020722@gmail.com>
2019-04-28 16:54:01 +0900

    [refactor]get_valueの関数類をfunctions.jsに移動した

--- game/functions.js
+++ game/functions.js
@@ -114,3 +114,77 @@ function get_value(game){
         game.count++;
     }
 }
+
+function get_value(){
+    old_x[count] = get_old_x();
+    old_y[count] = get_old_y();
+    new_x[count] = get_new_x();
+    new_y[count] = get_new_y();
+
+    count++;
+
+    if(count === agent_num){
+        game.agent_move(old_x, old_y, new_x, new_y,);
+        count = 0;
+        turn_count++;
+    }
+
+    if(turn_count === turn){
+        game.finish();
+    }
+}
+
+
+
+function get_team_number(){
+    var form = document.forms.set;
+
+    //blue OR orange
+    var team = form.team.value;
+
+    return parseInt(team);
+}
+
+function get_old_x(){
+    var form = document.forms.set;
+
+    var agent_number = parseInt(form.agent_No.value) + get_team_number();
+
+    for(var i=0; i<game.y; i++){
+        for(var j=0; j<game.x; j++){
+            if(game.board[i][j] === agent_number){
+                return j;
+            }
+        }
+    }
+}
+
+function get_old_y(){
+    var form = document.forms.set;
+
+    var agent_number = parseInt(form.agent_No.value) + get_team_number();
+
+    for(var i=0; i<game.y; i++){
+        for(var j=0; j<game.x; j++){
+            if(game.board[i][j] === agent_number){
+                return i;
+            }
+        }
+    }
+}
+
+function get_new_x(){
+    var form = document.forms.set;
+
+    var arr = form.dest.value.split(',');
+
+    return parseInt(arr[0]);
+}
+
+function get_new_y(){
+    var form = document.forms.set;
+
+    var arr = form.dest.value.split(',');
+
+    return parseInt(arr[1]);
+}
--- game/main.js
+++ game/main.js
@@ -1,10 +1,13 @@
 var point_map_num = parseInt(prompt("フィールド番号"));
-var turn = parseInt(prompt("TURN"));
 
 //map選択
 var point_map = choose_point_map(point_map_num);
 turn_count = 0;
 
+//TURN設定
+var turn = parseInt(prompt("TURN"));
+
+//x , y
 var y = point_map.length;
 var x = point_map[0].length;
 
@@ -12,8 +15,10 @@ var x = point_map[0].length;
 var agentsA = {0:0, 8:8},
 agentsB = {1:1, 9:9};
 
+//agent_num
 var agent_num = 0;
 for(let key in agentsA) agent_num++;
+agent_num *= 2;
 
 
 game = new Game(x, y, point_map, agentsA, agentsB, agent_num);
@@ -21,84 +26,8 @@ game = new Game(x, y, point_map, agentsA, agentsB, agent_num);
 game.draw_board();
 
 
-
 old_x = [];
 old_y = [];
 new_x = [];
 new_y = [];
 count = 0;
-
-
-function get_value(){
-    old_x[count] = get_old_x();
-    old_y[count] = get_old_y();
-    new_x[count] = get_new_x();
-    new_y[count] = get_new_y();
-
-    count++;
-
-    if(count === agent_num){
-        game.agent_move(old_x, old_y, new_x, new_y,);
-        count = 0;
-        turn_count++;
-    }
-
-    if(turn_count === turn){
-        game.finish();
-    }
-}
-
-
-
-function get_team_number(){
-    var form = document.forms.set;
-
-    //blue OR orange
-    var team = form.team.value;
-
-    return parseInt(team);
-}
-
-function get_old_x(){
-    var form = document.forms.set;
-
-    var agent_number = parseInt(form.agent_No.value) + get_team_number();
-
-    for(var i=0; i<game.y; i++){
-        for(var j=0; j<game.x; j++){
-            if(game.board[i][j] === agent_number){
-                return j;
-            }
-        }
-    }
-}
-
-function get_old_y(){
-    var form = document.forms.set;
-
-    var agent_number = parseInt(form.agent_No.value) + get_team_number();
-
-    for(var i=0; i<game.y; i++){
-        for(var j=0; j<game.x; j++){
-            if(game.board[i][j] === agent_number){
-                return i;
-            }
-        }
-    }
-}
-
-function get_new_x(){
-    var form = document.forms.set;
-
-    var arr = form.dest.value.split(',');
-
-    return parseInt(arr[0]);
-}
-
-function get_new_y(){
-    var form = document.forms.set;
-
-    var arr = form.dest.value.split(',');
-
-    return parseInt(arr[1]);
-}
